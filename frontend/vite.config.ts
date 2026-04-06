3640a7e5ec4cc54e310c3d5aba67bf5cc2b84a9d
Nazareno Aznar Altamiranda <nazareno.aznar@gmail.com>
2026-04-05 22:53:21 -0300

    refactor: remove default vite styling

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'url'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "url";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@shared': fileURLToPath(new URL('../shared', import.meta.url)),
+      "@shared": fileURLToPath(new URL("../shared", import.meta.url)),
     },
   },
-})
+});
